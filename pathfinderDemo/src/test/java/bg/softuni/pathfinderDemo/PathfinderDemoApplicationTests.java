292e1857248658e593b88b1609fad5c9896305aa
simoApostolov <simeon_apostolov@abv.bg>
2023-09-26 21:45:58 +0300

    Starting project

---
+++ pathfinderDemo/src/test/java/bg/softuni/pathfinderDemo/PathfinderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.pathfinderDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathfinderDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
