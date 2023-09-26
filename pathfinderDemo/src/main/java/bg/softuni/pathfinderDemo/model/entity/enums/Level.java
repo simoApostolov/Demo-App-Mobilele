064f91da991ed3864cb12cdb27cb4da47f6144c5
Simeon Atanasov <107774247+simoApostolov@users.noreply.github.com>
2023-09-26 22:50:03 +0300

    Delete pathfinderDemo/src/main/java/bg/softuni/pathfinderDemo/model/entity directory

--- pathfinderDemo/src/main/java/bg/softuni/pathfinderDemo/model/entity/enums/Level.java
+++
@@ -1,5 +0,0 @@
-package bg.softuni.pathfinderDemo.model.entity.enums;
-
-public enum Level {
-    BEGINNER, INTERMEDIATE, ADVANCED
-}
--- pathfinderDemo/src/main/java/bg/softuni/pathfinderDemo/model/entity/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package bg.softuni.pathfinderDemo.model.entity.enums;
-
-public enum Role {
-    ADMIN, MODERATOR,USER
-}
